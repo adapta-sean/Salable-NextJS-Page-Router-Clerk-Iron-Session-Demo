3b0fc8f16823e480c44cc87d5438732a803e0de3
Sean Cooper <scooper@adaptavist.com>
2024-06-03 13:41:50 +0100

    clean up

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -28,18 +28,18 @@ export default function Home() {
   }, [isSignedIn]);
 
   return (
-    <main
-      className={`flex min-h-screen flex-col py-6 px-24 ${inter.className}`}
-    >
-      <h1>Salable NextJS Page Router + Iron Session Demo</h1>
-      {session ? (
-          <>
-            <h2>Capabilities</h2>
-            <ul>
-              {session.capabilities.map(capability => <li key={capability}>{capability}</li>)}
-            </ul>
-          </>
-      ) : null}
-    </main>
+      <main
+          className={`flex min-h-screen flex-col py-6 px-24 ${inter.className}`}
+      >
+          <h1 className='text-2xl mb-4'>Salable NextJS Page Router + Clerk + Iron Session Demo</h1>
+          {session ? (
+              <>
+                  <h2 className='text-xl'>Capabilities</h2>
+                  <ul>
+                      {session.capabilities.map(capability => <li key={capability}>{capability}</li>)}
+                  </ul>
+              </>
+          ) : null}
+      </main>
   );
 }
